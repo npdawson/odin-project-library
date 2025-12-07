c0422cf6db60aac2611da8fd9873d748d29e69af
Natalie P. Dawson <natalie.p.dawson@gmail.com>
2025-12-07 11:16:00 -0700

    loop through books and add to table

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
+const myLibrary = [];
+
 class Book {
     constructor(title, author, pages) {
-        this.title = title
-        this.author = author
-        this.pages = pages
-        this.read = false
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = false;
+		this.id = crypto.randomUUID();
     }
 
 	info() {
-		var info = this.title + " by " + this.author + ", " + this.pages + " pages"
-		var readInfo = this.read ? ", read it" : ", not read yet"
-		return info + readInfo
+		var info = this.title + " by " + this.author + ", " + this.pages + " pages";
+		var readInfo = this.read ? ", read it" : ", not read yet";
+		return info + readInfo;
 	}
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295)
-console.log(theHobbit.info())
+function addBookToLibrary(title, author, pages, read) {
+	var book = new Book(title, author, pages);
+	book.read = read;
+	myLibrary.push(book);
+}
+
+function displayLibrary() {
+	const tbl = document.querySelector("#library");
+	const tbody = document.createElement("tbody");
+	tbl.appendChild(tbody);
+
+	myLibrary.forEach((book) => {
+		const row = document.createElement("tr");
+		const title = document.createElement("td");
+		title.appendChild(document.createTextNode(book.title));
+		row.appendChild(title);
+		const author = document.createElement("td");
+		author.appendChild(document.createTextNode(book.author));
+		row.appendChild(author);
+		const pages = document.createElement("td");
+		pages.appendChild(document.createTextNode(book.pages));
+		row.appendChild(pages);
+		const read = document.createElement("td");
+		const readYet = book.read ? "Read" : "Not read yet";
+		read.appendChild(document.createTextNode(readYet));
+		row.appendChild(read);
+		tbody.appendChild(row);
+	});
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+addBookToLibrary("Moby Dick", "Herman Melville", 688, false);
+
+displayLibrary();
